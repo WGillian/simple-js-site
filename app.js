bdfe3227cbbc685085d4926ae7ae591a33df3147
WGillian <whiteheadgillian@gmail.com>
2017-11-22 14:43:56 +0000

    changes-made

--- app.js
+++ app.js
@@ -6,9 +6,25 @@ var firstName = "Gillian";
 var lastName = "Whitehead";
 console.log(firstName+" "+lastName);
 
-console.log(lastName+", "+firstName);
+
+
+function firstNameLastName (firstName, lastName){
+	return firstName+" "+lastName;
+}
+console.log(firstNameLastName(firstName, lastName));
+
+
+
+function lastNameFirstName (firstName, lastName){
+	return lastName+", "+firstName;
+}
+console.log(lastNameFirstName(firstName, lastName));
+
 
 console.log("Number of letters in "+firstName+": "+firstName.length);
+
+
+
 
 var people = ['Jack', 'John', 'James'];
 people.map(function (names){console.log(names);});
@@ -20,48 +36,63 @@ var namesListed = function (names){
 	}
 	return list;
 };
-
 console.log(namesListed(people));
 
 
 
 
+var newPeople = ['Lucy', 'Jack', 'Roger', 'Jacob', 'Roxanne', 'James', 'Juliette', 'Jerome', 'Jessica', 'Rosa', 'Greg'];
 
-var removeFirstLetter = function (name){
-	return name.slice(1, name.length);
+
+var removeFirstLetter = function (names){
+	var list = "";
+	for(var i = 0; i < names.length; i++){
+	list += names[i].slice(1, names.length)+"\n";
+	}
+	return list;
 };
 
-console.log(removeFirstLetter(people[0]));
+console.log(removeFirstLetter(newPeople));
 
 
 
 
-
-var removeFirstLetterIfJ = function (name){
-	if(name.slice(0, 1) === "J"){
-	return name.slice(1, name.length);
-	} else {
-	return name
+var removeFirstLetterIfJ = function (names){
+	var list = "";
+	for(var i = 0; i < names.length; i++){
+		if(names[i].slice(0, 1) === "J"){
+		list += names[i].slice(1, names.length)+"\n";
+		} else {
+		list += names[i]+"\n";
+		}
 	};
+	return list;
+	
 };
 
-console.log(removeFirstLetterIfJ(people[1]));
+console.log(removeFirstLetterIfJ(newPeople));
 
 
 
 
-var task8 = function (name){
-	if(name.slice(0, 1) === "J"){
-	return name.slice(1, name.length);
-	} else if (name.slice(0, 1) === "R"){
-	return name.slice(0, name.length-2);
-	} else {
-	return name;
+
+
+var task8 = function (names){
+	var list = "";
+	for(var i = 0; i < names.length; i++){
+		if(names[i].slice(0, 1) === "J"){
+		list += names[i].slice(1, names[i].length)+"\n";
+		} else if (names[i].slice(0, 1) === "R"){
+		list += names[i].slice(0, names[i].length-2)+"\n";
+		} else {
+		list += names[i]+"\n";
+		}
 	};
+	return list;
+	
 };
 
-console.log(task8("Rupert"));
-
+console.log(task8(newPeople));
 
 
 
